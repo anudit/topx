4d8e7ab6c27ba16cf3bdf5dc8c1ae17035c61f30
Anudit Nagar <nagaranudit@gmail.com>
2019-05-07 23:40:32 +0530

    Generalize code for x images

--- app.py
+++ app.py
@@ -6,15 +6,26 @@ load_dotenv()
 APIKEY = os.environ.get("APIKEY")
 CX =  os.environ.get("CX")
 QUERY = "smile png"
+X = 12
 
-URL =  "https://www.googleapis.com/customsearch/v1?key="+APIKEY+"&cx="+CX+"&q="+QUERY+"&start=10"
-r = requests.get(URL)
-res = r.json()
-items = res['items']
+def getURLs(start = 0):  
+    urlList = []
+    
+    URL = "https://www.googleapis.com/customsearch/v1?key="+APIKEY+"&cx="+CX+"&q="+QUERY if start !=0 else "https://www.googleapis.com/customsearch/v1?key="+APIKEY+"&cx="+CX+"&q="+QUERY+"&start="+str(start+1)
+    r = requests.get(URL)
+    res = r.json()
+    items = res['items']
 
-for item in items:
-    pagemap = item['pagemap']
-    if('cse_image' in pagemap):
-        imageUrl = pagemap['cse_image'][0]['src']
-        print(imageUrl)
-        print("\n")
+    for item in items:
+
+        if('pagemap' in item and 'cse_image' in item['pagemap']):
+            imageUrl = item['pagemap']['cse_image'][0]['src']
+            urlList.append(imageUrl)
+
+    return urlList
+
+urlList = []
+for i in range(0, X, 10):
+    urlList += getURLs(i)
+
+print(urlList[0:X])
